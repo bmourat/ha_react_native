fc05291aa7c269ba04e7274a8b60ae1f415be158
Murat Baysangurov <v-mubays@microsoft.com>
2017-04-26 17:38:58 +0300

    Initial commit

---
+++ index.android.js
@@ -0,0 +1,62 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+var HockeyApp = require('react-native-hockeyapp');
+
+export default class hockeydemo extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );
+  }
+  componentWillMount() {
+    HockeyApp.configure('c3e4d3d3858145d4aaa909231cf24c17', true, HockeyApp.AuthenticationType.EmailPassword);
+  }
+
+  componentDidMount() {
+    HockeyApp.start();
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('hockeydemo', () => hockeydemo);
